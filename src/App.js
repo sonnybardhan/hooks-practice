81c5312ac96e6e6f9260d3c5fc3f7ff1b281a2d6
abhirup bardhan <bardhan.abhirup@gmail.com>
2022-08-10 18:22:25 +0530

    complete useAsync implementation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
-import './App.css';
+import useAsync from './hooks/useAsync';
+
+const URL = `https://jsonplaceholder.typicode.com/todos?_limit=10`;
+
+const fetchData = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const rand = Math.random();
+      if (rand < 0.5) {
+        resolve(`done! ${rand}`);
+      }
+      reject(`error! ${rand}`);
+    }, 1000);
+  });
+};
 
 function App() {
-  return <div className='App'>hello world</div>;
+  const { execute, status, error, value } = useAsync(fetchData, false);
+
+  return (
+    <div className='App'>
+      <h1>useAsync Demo</h1>
+      <div className='data-container'>
+        {status === 'success' && <div>{value}</div>}
+        {status === 'pending' && <div>Pending ... </div>}
+        {status === 'error' && <div>Oops! {error}</div>}
+      </div>
+      <button onClick={execute} disabled={status === 'pending'}>
+        Fetch
+      </button>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/hooks/useAsync.js
@@ -0,0 +1,37 @@
+import { useState, useEffect, useCallback } from 'react';
+
+function useAsync(func, immediate = true) {
+  const [status, setStatus] = useState('idle');
+  const [error, setError] = useState(null);
+  const [value, setValue] = useState(null);
+
+  const execute = useCallback(() => {
+    setStatus('pending');
+    setError(null);
+    setValue(null);
+
+    func()
+      .then((res) => {
+        setStatus('success');
+        setError(null);
+        setValue(res);
+        // console.log('res: ', res);
+      })
+      .catch((err) => {
+        setStatus('error');
+        setError(err);
+        setValue(null);
+        // console.log('err: ', err);
+      });
+  }, [func]);
+
+  useEffect(() => {
+    if (immediate) {
+      execute();
+    }
+  }, [execute, immediate]);
+
+  return { execute, status, error, value };
+}
+
+export default useAsync;
